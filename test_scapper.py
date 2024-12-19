b3b2ef0297ed706ca88d6211dd22dc2b49c338f5
Shubham Pratap Singh <shubham_pratap_singh_cw@lineaje.com>
2024-12-19 10:31:21 +0530

    Change in Test Case

--- test_scapper.py
+++ test_scapper.py
@@ -1,7 +1,7 @@
 import unittest
+from unittest.mock import patch, Mock
 from scraper import Scraper
 from storage import JSONFileStorage
-from cache import CacheManager
 from notifications import ConsoleNotifier
 import requests
 
@@ -22,32 +22,42 @@ class MockNotifier(ConsoleNotifier):
 class TestScraper(unittest.TestCase):
     def setUp(self):
         self.storage = MockStorage()
-        self.cache = CacheManager()
+        self.cache = Mock()
         self.notifier = MockNotifier()
         self.scraper = Scraper(storage_handler=self.storage, cache_manager=self.cache, notifier=self.notifier)
 
-    def test_scrape_single_page(self):
+
+    @patch('scraper.Scraper.download_image', return_value='/tmp/atly_images/image1.jpg')
+    def test_scrape_single_page(self, mock_download_image):
         # Mock HTML response
         def mock_get(url, headers=None, proxies=None):
             class MockResponse:
                 status_code = 200
-                def text(self):
-                    return """<div class='product-inner'>
+                text = """
+                    <!DOCTYPE html>
+                    <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <title>Basic HTML Page</title>
+                        </head>
+                        <body>
+                            <div class='product-inner'>
                                 <h3>Product 1</h3>
                                 <span class='woocommerce-Price-amount'>$10.0</span>
                                 <a><img src='http://example.com/image1.jpg' title="Product 1"/></a>
-                            </div>"""
+                            </div>
+                        </body>
+                    </html>
+                """
             return MockResponse()
 
+        self.cache.is_price_changed.return_value = True
         requests.get = mock_get
         self.scraper.scrape(max_pages=1)
         self.assertEqual(len(self.storage.data), 1)
         self.assertEqual(self.notifier.message, "Scraping completed. 1 products updated in the database.")
 
-    def test_cache_prevention(self):
-        self.cache.is_price_changed("Product 1", 10.0)
-        self.assertFalse(self.cache.is_price_changed("Product 1", 10.0))
-        self.assertTrue(self.cache.is_price_changed("Product 1", 20.0))
 
 if __name__ == "__main__":
     unittest.main()
